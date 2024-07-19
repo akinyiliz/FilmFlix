7a4f6bf15b319282f0e842414816f7f592a2e6d8
akinyiliz <lizzieke84@gmail.com>
2024-07-19 22:33:08 +0300

    feat: Implement function to get TV Shows

--- components/MoviesContainer.tsx
+++ components/MoviesContainer.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import Image from "next/image";
+import Movie from "@/types/movie";
 import React, { useEffect, useState } from "react";
-
-type Movie = {
-  id: number;
-  title: string;
-  backdrop_path: string;
-  poster_path: string;
-  first_air_date: string;
-  vote_average: string;
-  genre_ids: number[];
-};
+import MovieCard from "./MovieCard";
+import Tab from "./Tab";
+import Show from "@/types/show";
 
 function MoviesContainer() {
   const [movies, setMovies] = useState<Movie[]>([]);
-  const [shows, setShows] = useState();
+  const [shows, setShows] = useState<Show[]>([]);
+  const [activeTab, setActiveTab] = useState("movies");
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -32,32 +26,64 @@ function MoviesContainer() {
     fetchMovies();
   }, []);
 
-  console.log(movies);
+  useEffect(() => {
+    const fetchTvShows = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+        );
 
+        const data = await response.json();
+        setShows(data.results);
+      } catch (error) {}
+    };
+
+    fetchTvShows();
+  }, []);
+
+  // console.log(movies);
+  // console.log("Movies", shows);
   return (
     <div className="">
       <div className="bg-[#101625] w-fit flex items-center gap-3 p-3 rounded-xl">
-        <button className="bg-[#7c6ef6] text-[#c8d6d7] font-bold px-6 py-2 rounded-lg">
-          Movies
-        </button>
-        <button className="font-bold px-6 py-2">TV Shows</button>
+        <Tab
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          tabName="movies"
+        />
+        <Tab
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          tabName="TV shows"
+        />
       </div>
 
       <div className="grid grid-cols-2 my-4 gap-8 md:grid-cols-3 lg:grid-cols-4">
-        {movies.map((movie) => (
-          <div key={movie.id}>
-            <Image
-              src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`}
-              alt={movie.title}
-              width={400}
-              height={400}
-              layout="responsive"
-              className="rounded-lg"
-              loading="lazy"
-            />
-            <p>{movie.title}</p>
-          </div>
-        ))}
+        {activeTab === "movies" ? (
+          <>
+            {movies.map((movie) => (
+              <MovieCard
+                key={movie.id}
+                id={movie.id}
+                title={movie.title}
+                poster_path={movie.poster_path}
+                vote_average={movie.vote_average}
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            {shows.map((show) => (
+              <MovieCard
+                key={show.id}
+                id={show.id}
+                title={show.name}
+                poster_path={show.poster_path}
+                vote_average={show.vote_average}
+              />
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
