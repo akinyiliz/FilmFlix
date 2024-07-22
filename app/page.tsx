619afd6c609d93ad1425696e8d4f22671657965c
akinyiliz <lizzieke84@gmail.com>
2024-07-22 19:53:25 +0300

    chore: Remove Navbar from main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/Hero";
 import MoviesContainer from "@/components/MoviesContainer";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="bg-[#121829] text-[#666e83] min-h-screen">
+    <main className="mt-16 min-h-screen">
       <div className="flex flex-col px-4 md:px-6 lg:max-w-6xl lg:mx-auto">
-        <Navbar />
         <Hero />
         <MoviesContainer />
       </div>
