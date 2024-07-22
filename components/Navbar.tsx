97bce135c69590445113cfec738df02e6d7a3ed0
akinyiliz <lizzieke84@gmail.com>
2024-07-22 19:54:12 +0300

    style: Make Navbar fixed and have a blur background

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="flex justify-between items-center py-4">
-      <h1 className="text-[#c8d6d7] font-extrabold text-3xl">FilmFlix</h1>
+    <div className=" fixed top-0 left-0 right-0 z-10 bg-[#121829]/80 backdrop-blur-md p-4 md:px-6">
+      <div className="flex justify-between items-center  lg:max-w-6xl lg:mx-auto">
+        <h1 className="text-[#c8d6d7] font-extrabold text-3xl">FilmFlix</h1>
 
-      <button className="font-bold text-xl">My Watchlist</button>
+        <button className="font-bold text-xl">My Watchlist</button>
+      </div>
     </div>
   );
 }
