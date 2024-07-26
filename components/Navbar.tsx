79317171df8220612bb3770d515d94e9cf79f308
akinyiliz <lizzieke84@gmail.com>
2024-07-26 15:31:43 +0300

    feat: Change button to link tag to watchlist page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 function Navbar() {
   return (
     <div className=" fixed top-0 left-0 right-0 z-10 bg-[#121829]/80 backdrop-blur-md p-4 md:px-6">
       <div className="flex justify-between items-center  lg:max-w-6xl lg:mx-auto">
-        <h1 className="text-[#c8d6d7] font-extrabold text-3xl">FilmFlix</h1>
+        <Link href="/" className="text-[#c8d6d7] font-extrabold text-3xl">
+          FilmFlix
+        </Link>
 
-        <button className="font-bold text-xl">My Watchlist</button>
+        <Link href="/watchlist" className="font-bold text-xl">
+          My Watchlist
+        </Link>
       </div>
     </div>
   );
