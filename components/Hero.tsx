6cb36a6711d23124a75ad24fb1e6a47321570312
akinyiliz <lizzieke84@gmail.com>
2024-08-01 21:14:26 +0300

    feat: Add handleSearch functionality

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,20 @@
-import React from "react";
-import Searchbar from "./Searchbar";
+"use client";
 
-function Hero() {
+import React, { FC, FormEventHandler, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+type HeroProps = {
+  onSearch: (query: string) => void;
+};
+
+const Hero: FC<HeroProps> = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch: FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+    onSearch(searchTerm);
+  };
+
   return (
     <div className="my-20 w-full md:w-3/4 lg:w-1/2 space-y-3">
       <p className="text-[#6d758b] text-lg">
@@ -15,9 +28,21 @@ function Hero() {
         about each movie and make informed choices.
       </p>
 
-      <Searchbar />
+      <div className="bg-[#101625] flex items-center gap-3 border border-[#1b2236] p-4 rounded-xl">
+        <FaSearch size={22} />
+
+        <form onSubmit={handleSearch} className="w-full">
+          <input
+            type="text"
+            placeholder="Search movies or tv shows"
+            className="bg-transparent w-full focus:outline-none"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default Hero;
