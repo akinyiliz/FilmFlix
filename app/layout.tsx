299da95f0325e999e3a633a2d8aaa38a82f3a8a9
akinyiliz <lizzieke84@gmail.com>
2024-08-01 21:12:14 +0300

    chore: Wrap app with WatchlistProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { WatchlistProvider } from "@/context/WatchlistContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,9 +23,11 @@ export default function RootLayout({
       <body
         className={`${inter.className} min-h-screen bg-[#121829] text-[#666e83] flex flex-col`}
       >
-        <Navbar />
-        {children}
-        <Footer />
+        <WatchlistProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </WatchlistProvider>
       </body>
     </html>
   );
