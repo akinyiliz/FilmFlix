b4dd927e7eb74960b07a4e8f360bdb45fb7c6c48
akinyiliz <lizzieke84@gmail.com>
2024-08-11 14:34:23 +0300

    feat: Add number of items in watchlist

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { useWatchlist } from "@/context/WatchlistContext";
 
 function Navbar() {
+  const { watchlist } = useWatchlist();
+
   return (
     <div className=" fixed top-0 left-0 right-0 z-10 bg-[#121829]/80 backdrop-blur-md p-4 md:px-6">
       <div className="flex justify-between items-center  lg:max-w-6xl lg:mx-auto">
@@ -9,8 +14,8 @@ function Navbar() {
           FilmFlix
         </Link>
 
-        <Link href="/watchlist" className="font-bold text-xl">
-          My Watchlist
+        <Link href="/watchlist" className="relative font-bold text-xl">
+          Watchlist{watchlist.length > 0 && <span>({watchlist.length})</span>}
         </Link>
       </div>
     </div>
