d419cc66440fb41fea88bfca88c463b02355a444
akinyiliz <lizzieke84@gmail.com>
2024-08-11 14:38:38 +0300

    feat: Creat watchlist context consisting of the watchlist, addToWatchlist and removeFromWatchlist functionalities

---
+++ context/WatchlistContext.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  useEffect,
+} from "react";
+import Movie from "@/types/movie";
+
+type WatchlistContextType = {
+  watchlist: Movie[];
+  addToWatchlist: (item: Movie) => void;
+  removeFromWatchlist: (id: number) => void;
+};
+
+const WatchlistContext = createContext<WatchlistContextType | undefined>(
+  undefined
+);
+
+export const WatchlistProvider = ({ children }: { children: ReactNode }) => {
+  const [watchlist, setWatchlist] = useState<Movie[]>(() => {
+    const storedWatchlist = localStorage.getItem("watchlist");
+    return storedWatchlist ? JSON.parse(storedWatchlist) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(watchlist));
+  });
+
+  const addToWatchlist = (item: Movie) => {
+    setWatchlist((prevWatchlist) => {
+      const isAlreadyInWatchlist = prevWatchlist.some(
+        (watchlistItem) => watchlistItem.id === item.id
+      );
+
+      if (!isAlreadyInWatchlist) {
+        return [...prevWatchlist, item];
+      }
+
+      return prevWatchlist;
+    });
+  };
+
+  const removeFromWatchlist = (id: number) => {
+    setWatchlist((prevWatchlist) =>
+      prevWatchlist.filter((item) => item.id !== id)
+    );
+  };
+
+  return (
+    <WatchlistContext.Provider
+      value={{ watchlist, addToWatchlist, removeFromWatchlist }}
+    >
+      {children}
+    </WatchlistContext.Provider>
+  );
+};
+
+export const useWatchlist = () => {
+  const context = useContext(WatchlistContext);
+  if (context === undefined) {
+    throw new Error("useWatchlist must be used within a WatchlistProvider");
+  }
+  return context;
+};
